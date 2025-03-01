791b6b8b449fd9721f5428431abd1ff5ee423f51
faridamousaa <faridamousa36@gmail.com>
2025-03-01 13:41:47 +0200

    order model done

--- src/main/java/com/example/model/Order.java
+++ src/main/java/com/example/model/Order.java
@@ -1,4 +1,64 @@
 package com.example.model;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
 public class Order {
+    private UUID id;
+    private UUID userId;
+    private double totalPrice;
+    private List<Product> products=new ArrayList<>();
+
+    //constructors
+    public Order() {
+
+    }
+
+    public Order(UUID id, UUID userId, double totalPrice, List<Product> products) {
+        this.id = id;
+        this.userId = userId;
+        this.totalPrice = totalPrice;
+        this.products = products;
+    }
+
+    public Order(UUID userId, double totalPrice, List<Product> products) {
+        this.userId = userId;
+        this.totalPrice = totalPrice;
+        this.products = products;
+    }
+
+    //getters and setters
+    //id
+    public UUID getId() {
+        return id;
+    }
+    public void setId (UUID id) {
+        this.id = id;
+    }
+
+    //userId
+    public UUID getUserId() {
+        return userId;
+    }
+    public void setUserId (UUID userId) {
+        this.userId = userId;
+    }
+
+    //totalPrice
+    public double getTotalPrice() {
+        return totalPrice;
+    }
+    public void setTotalPrice(double totalPrice) {
+        this.totalPrice = totalPrice;
+    }
+
+    //products
+    public List<Product> getProducts() {
+        return products;
+    }
+    public void setProducts(List<Product> products) {
+        this.products = products;
+    }
+
 }
