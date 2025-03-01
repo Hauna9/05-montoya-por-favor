69bd20ff02bbb9bfed3c8496fed203e5f27dcc9f
faridamousaa <faridamousa36@gmail.com>
2025-03-01 14:46:39 +0200

    order repository done

--- src/main/java/com/example/repository/OrderRepository.java
+++ src/main/java/com/example/repository/OrderRepository.java
@@ -1,4 +1,49 @@
 package com.example.repository;
 
-public class OrderRepository {
+import com.example.model.Order;
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.NoSuchElementException;
+import java.util.UUID;
+
+@Repository
+@SuppressWarnings("rawtypes")
+
+public class OrderRepository extends MainRepository<Order> {
+
+    @Override
+    protected String getDataPath() {
+        return "src/main/java/com.example/data/orders.json";
+    }
+
+    @Override
+    protected Class<Order[]> getArrayType() {
+        return Order[].class;
+    }
+
+    public void addOrder(Order order) {
+        save(order);
+    }
+
+    public ArrayList<Order> getOrders() {
+        return findAll();
+    }
+
+    public Order getOrderById(UUID orderId){
+        ArrayList<Order> orders = getOrders();
+        for (Order order : orders) {
+            if(order.getId().equals(orderId)){
+                return order;
+            }
+        }
+        throw new NoSuchElementException("Order with ID " + orderId + " not found.");
+    }
+
+    public void deleteOrderById(UUID orderId){
+        ArrayList<Order> orders = getOrders();
+        orders.removeIf(order -> order.getId().equals(orderId));
+        saveAll(orders);
+    }
+
 }
