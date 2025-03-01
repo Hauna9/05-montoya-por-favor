49c8991ba4c2ccfab4550a2ac9f81ef5f4752d6f
faridamousaa <faridamousa36@gmail.com>
2025-03-01 15:34:19 +0200

    order service done

--- src/main/java/com/example/service/OrderService.java
+++ src/main/java/com/example/service/OrderService.java
@@ -1,4 +1,47 @@
 package com.example.service;
 
-public class OrderService {
+import com.example.model.Order;
+import com.example.repository.OrderRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.ArrayList;
+import java.util.UUID;
+
+@Service
+@SuppressWarnings("rawtypes")
+public class OrderService extends MainService<Order> {
+
+    OrderRepository orderRepository;
+    @Autowired
+    public OrderService(OrderRepository orderRepository) {
+        this.orderRepository = orderRepository;
+    }
+
+    public void addOrder(Order order){
+        orderRepository.addOrder(order);
+    }
+
+    public ArrayList<Order> getOrders(){
+        return orderRepository.getOrders();
+    }
+
+    public Order getOrderById(UUID orderId){
+        Order order = orderRepository.getOrderById(orderId);
+        if(order == null){
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "User not found");
+        }
+        return order;
+    }
+
+    public void deleteOrderById(UUID orderId) throws IllegalArgumentException{
+        Order order = orderRepository.getOrderById(orderId);
+        if(order == null){
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "User not found");
+        }
+        orderRepository.deleteOrderById(orderId);
+    }
+
 }
